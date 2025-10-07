a968a395b391c3f97e6835c3535a3b0ee7f58cef
hyperGG32 <196324859+hyperGG32@users.noreply.github.com.>
2025-10-07 10:05:32 +0300

    task 2 done

--- task02_squares_mapping_task.py
+++ task02_squares_mapping_task.py
@@ -10,3 +10,5 @@ create a dictionary where key is the number and value is its square.
 # Starter:
 nums = [1, 2, 3, 4, 5]
 # TODO: create dict of squares; print it
+squares_dict = {i: i**2 for i in nums}
+print(squares_dict)
