8c97f329e844c27e46692254fbef697f0dd43e1e
hyperGG32 <196324859+hyperGG32@users.noreply.github.com.>
2025-10-07 10:29:00 +0300

    task 7 done

--- task07_car_colors_group_task.py
+++ task07_car_colors_group_task.py
@@ -12,7 +12,7 @@ cars = [("BMW","black"), ("Audi","red"), ("BMW","white"), ("Audi","blue")]
 # TODO: build brand -> list_of_colors dict and print it
 cars_dict = {}
 for Tuple in cars:
-    if cars_dict.get(Tuple[0]) == None:
+    if cars_dict.get(Tuple[0]) is None:
         cars_dict[Tuple[0]] = []
         cars_dict[Tuple[0]].append(Tuple[1])
     else:
