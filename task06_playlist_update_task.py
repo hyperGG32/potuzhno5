b6fab559cf774ee82a298df9a79c95e1dfb0d799
hyperGG32 <196324859+hyperGG32@users.noreply.github.com.>
2025-10-07 10:18:29 +0300

    task 6 done

--- task06_playlist_update_task.py
+++ task06_playlist_update_task.py
@@ -10,3 +10,14 @@ Add one new artist to each genre list, then print all genres and artists.
 # Starter:
 playlist = {"rock": ["Queen", "AC/DC"], "pop": ["Adele"], "jazz": []}
 # TODO: append one artist per genre; then print genre -> list
+playlist["rock"].append("DJ Montagem")
+playlist["pop"].append("DJ Spoiler")
+playlist["jazz"].append("Garik")
+for genre in playlist:
+    print(genre.capitalize() + ": ", end='')
+    l = playlist[genre]
+    for i in l:
+        if i == l[-1]:
+            print(i, end='.\n')
+            continue
+        print(i, end=", ")
